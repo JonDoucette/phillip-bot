d4fb0525ee6490abf667afca53a1c3c1c72bd5ea
Jon Doucette <jonsgames@hotmail.com>
2020-05-29 08:51:40 -0700

    Initial commit, discord bot read from JSON file

---
+++ bot.py
@@ -0,0 +1,29 @@
+import discord
+import json
+import random
+
+client = discord.Client()
+
+def randomQuote():
+
+	f = open('json.json')
+
+	data = json.load(f)
+
+	response = ''
+
+	while len(response) < 8:
+		number = random.randint(0, len(data['messages']))
+		response = str(data['messages'][number]['content'])
+
+	return (response)
+
+
+@client.event
+async def on_message(message):
+	if message.content.find("!quote") != -1:
+		await message.channel.send(randomQuote())
+
+
+client.run('NzE1ODExMjE3OTAxNjE3MTUy.XtCpSA.AgiLs1GN8JbPzUCcKnIp6Qk7bWs')
+
